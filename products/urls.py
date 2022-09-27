46b5f49108428f0725a2e8443a63696b0a3673cd
temesgen-zenebe <temf2006@gmail.com>
2022-09-26 20:12:04 -0700

    editing product detail page

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import * 
 from .views import (ProductsListView , ProductsDetailViews,)
 
-app_name = 'products'
+app_name = 'products' 
 urlpatterns = [
     path('product', ProductsListView.as_view(), name="product-list"),
     path('product/<slug:slug>', ProductsDetailViews.as_view(), name="product-detail"),
