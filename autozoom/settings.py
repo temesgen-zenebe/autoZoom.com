762b9170206d5aec97ca0721ad803269ce1ca82a
temesgen-zenebe <temf2006@gmail.com>
2022-09-28 21:53:20 -0700

    document creation seating

--- autozoom/settings.py
+++ autozoom/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     
     # Built-in Django apps
     'django.contrib.admin',
+    'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
