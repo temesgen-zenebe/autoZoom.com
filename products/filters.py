643b6f381c84daac8b7641fbc5105f10136a220f
temesgen-zenebe <temf2006@gmail.com>
2023-01-04 04:00:15 -0800

    filter_by_order added

--- products/filters.py
+++ products/filters.py
@@ -3,9 +3,17 @@ from .models import Product
 
 
 class ProductFilter(django_filters.FilterSet):
+    CHOICES = (
+        ('ascending', 'Ascending'),
+        ('descending','Descending')
+        )
+    ordering = django_filters.ChoiceFilter(label='Ordering' , choices=CHOICES, method='filter_by_order')
+    
     
     class Meta:
         model = Product 
         fields = ('product_name','part_number', 'brand','tags','category')
         
-    
+    def filter_by_order(self, queryset,name ,value):
+        expression = 'created' if value =='ascending' else '-created'
+        return queryset.order_by(expression)
